e653eaf9c9dbed3a11649784dca2cdc9a3436406
Abdullrahman <abdullrahmanabdullah59@gmail.com>
2026-05-05 12:35:29 +0300

    .env virabels added

--- core/config.py
+++ core/config.py
@@ -11,7 +11,10 @@ load_dotenv()
 # --- إعدادات Supabase ---
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-# تهيئة عميل Supabase المركزي
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise ValueError("CRITICAL: SUPABASE_URL or SUPABASE_KEY is missing from Environment Variables!")
+
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # --- إعدادات Firebase ---
